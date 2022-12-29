ab719f7d617719145680d12ea90264e875c6e536
DizzlyD <david4henery00@hotmail.com>
2022-12-29 20:40:07 +0000

    added-pricing-data

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { CheckIcon } from "../assets";
+import pricingPlan from "../pricing-plan-data/pricingPlan";
 
 const Pricing = () => {
   return (
@@ -10,73 +11,44 @@ const Pricing = () => {
         </h2>
       </div>
       {/* PRICING PLANS */}
-      <div>
+      <div className="flex flex-col justify-center items-center gap-12">
         {/* PRICING PLAN */}
-        <div className="flex flex-col justify-center items-center gap-6">
-          <div>
-            <h3 className="text-2xl font-medium font-dmDisplay text-primaryDarkPink">
-              Free Plan
-            </h3>
-            <p className="pt-4 pb-2 text-secondaryLightSanJuanBlue font-normal text-[15px]">
-              Build and test using our core set of products with up to 100 API
-              requests{" "}
-            </p>
-            <p className="text-[56px] text-secondarySanJuanBlue font-medium font-dmDisplay">
-              $0.00
-            </p>
+        {pricingPlan.map((plan) => (
+          <div key={plan.id} className="flex flex-col justify-center items-center gap-6">
+            <div>
+              <h3 className="text-2xl font-medium font-dmDisplay text-primaryDarkPink">
+                {plan.planType}
+              </h3>
+              <p className="pt-4 pb-2 text-secondaryLightSanJuanBlue font-normal text-[15px]">
+                {plan.planDescription}
+              </p>
+              <p className="text-[56px] text-secondarySanJuanBlue font-medium font-dmDisplay">
+                {plan.price}
+              </p>
+            </div>
+            <div className="w-full border-y border-y-secondarySanJuanBlue/25 grid place-items-center">
+              <ul className="py-12 text-left flex flex-col gap-2">
+                {plan.features.map(features => {
+                  const {id, isIncluded, name} = features
+                  return (
+                    <li
+                      key={id}
+                      className="flex justify-start items-center gap-6"
+                    >
+                      <span>{isIncluded && <CheckIcon />}</span>
+                      <p>{name}</p>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div>
+              <button className="secondary-query-btn w-[168px]">
+                Request Access
+              </button>
+            </div>
           </div>
-          <div className="w-full border-y border-y-secondarySanJuanBlue/25 grid place-items-center">
-            <ul className="py-12 text-left flex flex-col gap-2">
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Transactions</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Auth</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Identity</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Investments</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Assets</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Liabilities</p>
-              </li>
-              <li className="flex justify-start items-center gap-6">
-                <span>
-                  <CheckIcon />
-                </span>
-                <p>Income</p>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <button className="secondary-query-btn w-[168px]">
-              Request Access
-            </button>
-          </div>
-        </div>
+        ))}
         {/**/}
       </div>
     </section>
