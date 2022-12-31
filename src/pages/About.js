a2984ed36c0f011add3ea8ef1a49c1296369f192
DizzlyD <david4henery00@hotmail.com>
2022-12-31 11:01:41 +0000

    about-page-fully-responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <>
       {/* TOP SECTION */}
-      <section className="col-start-2 col-end-12 flex flex-col justify-center items-center gap-10 pt-12 pb-[76px] md:gap-14 md:pt-[75px] md:max-w-[920px] md:mx-auto">
-        <h2 className="text-[32px] leading-9 text-secondarySanJuanBlue font-medium font-dmDisplay text-center md:text-5xl lg:text-left">
+      <section className="col-start-2 col-end-12 flex flex-col justify-center items-center gap-10 pt-12 pb-[76px] md:gap-14 md:pt-[75px] md:max-w-[920px] md:mx-auto lg:items-start lg:pt-[82px] lg:pb-[100px]">
+        <h2 className="text-[32px] leading-9 text-secondarySanJuanBlue font-medium font-dmDisplay text-center md:text-5xl lg:text-left lg:text-[56px] lg:max-w-[660px]">
           We empower innovators by delivering access to the financial system
         </h2>
-        <div className="flex flex-col justify-center items-center gap-12 text-center">
+        <div className="flex flex-col justify-center items-center gap-12 text-center md:gap-16">
           <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:items-start md:w-full md:justify-start md:text-left md:gap-[10px]">
             <h3 className="text-2xl font-dmDisplay text-secondarySanJuanBlue md:min-w-[223px] md:text-[32px]">
               Our Vision
@@ -36,7 +36,7 @@ const About = () => {
         </div>
       </section>
       {/* IMAGE SECTION */}
-      <div className="col-start-1 col-end-13 pb-12">
+      <div className="col-start-1 col-end-13 pb-12 md:pb-16 lg:pb-[72px]">
         <picture>
           <source srcSet={`${teamImgDesk} 2x`} media="(min-width:64em)" />
           <source srcSet={`${teamImgTab} 2x`} media="(min-width:31.25em)" />
@@ -49,7 +49,7 @@ const About = () => {
         </picture>
       </div>
       {/* BOTTOM SECTIONS */}
-      <section className="py-4 border-y border-y-secondarySanJuanBlue/25 col-start-2 col-end-12 flex flex-col justify-center items-center gap-8 md:flex-row md:gap-[10px] md:border-y-0">
+      <section className="py-4 border-y border-y-secondarySanJuanBlue/25 col-start-2 col-end-12 flex flex-col justify-center items-center gap-8 md:flex-row md:gap-[10px] md:border-y-0 lg:gap-[30px]">
         <div className="grid place-items-center md:w-full md:justify-items-start md:border-y md:border-y-secondarySanJuanBlue/25 md:py-4">
           <h4>Team Members</h4>
           <p className="text-[56px] text-primaryDarkPink font-dmDisplay leading-[72px]">
@@ -70,7 +70,7 @@ const About = () => {
         </div>
       </section>
       {/**/}
-      <section className="col-start-2 col-end-12 flex flex-col justify-center items-center gap-12 text-center pt-12 md:max-w-[920px] md:mx-auto">
+      <section className="col-start-2 col-end-12 flex flex-col justify-center items-center gap-12 text-center pt-12 md:max-w-[920px] md:mx-auto md:gap-16 md:pb-16 lg:pb-[72px]">
         <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:items-start md:w-full md:justify-start md:text-left md:gap-[10px]">
           <h3 className="text-2xl font-dmDisplay text-secondarySanJuanBlue md:min-w-[223px] md:text-[32px]">
             The Culture
