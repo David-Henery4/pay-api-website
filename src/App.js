e70e12aa98b935a32fae985a45da21f401cbdf7d
DizzlyD <david4henery00@hotmail.com>
2023-01-03 12:29:29 +0000

    scroll-to-top-wrapper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
 import { Navbar, Footer, Sidebar } from "./components";
-import { Outlet } from "react-router-dom";
-import {SharedCta} from "./pages/SharedSections";
+import { Outlet, useLocation } from "react-router-dom";
+import { SharedCta } from "./pages/SharedSections";
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children
+};
 
 function App() {
-  const [isSidebarOpen,setIsSidebarOpen] = useState(false)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <main className="App bg-mainBgChilledWhite text-secondaryLightSanJuanBlue grid grid-cols-mobColumns sm:grid-cols-tabColumns xl:grid-cols-deskColumns overflow-hidden">
-      {/* NAVBAR */}
-      <Navbar setIsSidebarOpen={setIsSidebarOpen} />
-      <Sidebar
-        isSidebarOpen={isSidebarOpen}
-        setIsSidebarOpen={setIsSidebarOpen}
-      />
-      {/* PAGE CONTENT */}
-      <Outlet/>
-      {/* CTA SECTION */}
-      <SharedCta/>
-      {/* FOOTER */}
-      <Footer />
+      <Wrapper>
+        {/* NAVBAR */}
+        <Navbar setIsSidebarOpen={setIsSidebarOpen} />
+        <Sidebar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        {/* PAGE CONTENT */}
+        <Outlet />
+        {/* CTA SECTION */}
+        <SharedCta />
+        {/* FOOTER */}
+        <Footer />
+      </Wrapper>
     </main>
   );
 }
