15c9550962b016d981772959ea2aa65d01c01d58
DizzlyD <david4henery00@hotmail.com>
2023-01-04 14:37:16 +0000

    testing-commit-2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Logo, TwitterIcon, FacebookIcon, LinkedinIcon, BgPattern } from "../assets";
+import {
+  Logo,
+  TwitterIcon,
+  FacebookIcon,
+  LinkedinIcon,
+  BgPattern,
+} from "../assets";
 import BgPatternWrapper from "../assets/cloneBgPattern";
 import navData from "../navigation/navData";
 
